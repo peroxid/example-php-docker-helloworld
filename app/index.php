b64fb01740ec33fe77919aa3b77ee72e9ebe82eb
peroxid <peroxid@users.noreply.github.com>
2018-11-12 15:16:23 +0100

    Update index.php

--- app/index.php
+++ app/index.php
@@ -53,7 +53,7 @@
     <div class="container">
 
       <div class="starter-template">
-        <h1>Old Man yells at cloud</h1>
+        <h1>Old Man yells at cloud maimai</h1>
         <p class="lead">APPUiO Example Dockerfile PHP</p>
       </div>
 
